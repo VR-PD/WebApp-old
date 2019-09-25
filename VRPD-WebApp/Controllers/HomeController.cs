b69f401627f96bce07e7546c40c72056086b9c43
Bram Pieters <s1110012@student.windesheim.nl>
2019-09-25 12:19:38 +0200

    Formatting

--- VRPD-WebApp/Controllers/HomeController.cs
+++ VRPD-WebApp/Controllers/HomeController.cs
@@ -8,11 +8,6 @@ namespace VRPD_WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -24,6 +19,11 @@ namespace VRPD_WebApp.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Index()
+        {
             return View();
         }
     }
